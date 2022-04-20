dc6c31d4d3b21846a13afdd8d30bb517fdabf5a3
refa3e99 <refa3e99@icloud.com>
2022-04-20 18:37:57 +0300

    3 more questions

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
-var name = prompt("Please enter your name");
-var gender = prompt("please enter your gender (male/female)");
-var age = prompt("how old are you?");
+let name = prompt("Please enter your name");
+let gender = prompt("please enter your gender (male/female)");
+let age = prompt("how old are you?");
 
-var result = confirm("do you want to skip the welcoming message?")
+let result = confirm("do you want to skip the welcoming message?")
 if (result==true){
     alert("skipped welcoming message");
 }
 else {
-var userGender;
+let userGender;
 
 if (age <= 0) {
     alert("The age is less than or equal to zero!");
@@ -23,3 +23,52 @@ if (gender == "male") {
     userGender = " ";
   }
 alert("Welcome "+userGender+name);}
+
+let answers = [];
+let q1,q2,q3;
+
+function question1(){
+q1 = prompt("do u work?");
+  isValid(q1);
+
+  function isValid(answer){
+    while(answer != "yes" && answer != "no"){
+        answer = alert("invalid input");
+        question1();
+        break;
+    }}
+
+  }
+
+  function question2(){
+    q2 = prompt("do u live in jordan?");
+    isValid(q2);
+  
+    function isValid(answer){
+      while(answer != "yes" && answer != "no"){
+          answer = alert("invalid input");
+          question2();
+          break;
+      }}
+      
+    }
+
+    function question3(){
+    q3 = prompt("are u a human?");
+      isValid(q3);
+    
+      function isValid(answer){
+        while(answer != "yes" && answer != "no"){
+            answer = alert("invalid input");
+            question3();
+            break;
+        }}
+        
+      }
+  
+  question1();
+  question2();
+  question3();
+
+  answers.push(q3,q2,q1);
+  console.log(answers);
